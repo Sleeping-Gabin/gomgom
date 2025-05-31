4ffb25ec7d46610995aedbab32287d55ea858e34
bini <bini020408@naver.com>
2025-06-01 02:00:16 +0900

    상세 페이지 옵션에서 클릭 이벤트로 드롭다운 메뉴를 여닫고, 선택 시 텍스트가 변경되게 함

--- detail_page/js/detail.js
+++ detail_page/js/detail.js
@@ -1,5 +1,7 @@
 $(function() {
   detailImgEvent();
+
+  controlOption();
   
   productEvent();
   
@@ -19,6 +21,86 @@ function detailImgEvent() {
     let imgUrl = $(this).children("img").attr("src");
     bigImg.css("background-image", `url("${imgUrl}")`);
   });
+}
+
+function controlOption() {
+  const option = $(".product-detail-section .purchase-detail-container .detail-option");
+  const dropdowns = $(".product-detail-section .purchase-detail-container .detail-option .dropdown");
+  const dropdownList = $(".product-detail-section .purchase-detail-container .detail-option .dropdown .dropdown-menu li");
+
+  //선택 가능한 옵션 메뉴 클릭시 메뉴를 보이거나 닫음
+  option.on("click", ".dropdown.selectable", function() {
+    let currentHeight = $(this).children(".dropdown-menu").innerHeight();
+
+    if (currentHeight == 0) {
+      closeMenu(dropdowns);
+      openMenu($(this));
+    }
+    else {
+      closeMenu($(this));
+    }
+  });
+
+  //메뉴 선택시
+  dropdownList.on("click", function() {
+    const dropdown = $(this).parents(".dropdown");
+
+    let idx = dropdowns.index(dropdown);
+    let selectText = $(this).text();
+
+    //선택 메뉴 표시
+    dropdown.children("p").text(selectText);
+
+    if (idx < dropdowns.length-1) {
+      //다음 옵션을 선택 가능하게 하고, 이후 옵션들을 초기화
+      dropdowns.eq(idx+1).addClass("selectable");
+      dropdowns.slice(idx+2).removeClass("selectable");
+      clearMenuText(dropdowns.slice(idx+1));
+    }
+    else {
+      clearOptions(dropdowns);
+    }
+  });
+
+  //메뉴를 엶
+  function openMenu(menu) {
+    let menuHeight = menu.find(".dropdown-menu ul").outerHeight();
+
+    menu.children(".dropdown-close-btn").show();
+    menu.children(".dropdown-open-btn").hide();
+    menu.children(".dropdown-menu").animate(
+      { height: menuHeight },
+      100
+    );
+  }
+
+  //메뉴를 닫음
+  function closeMenu(menu) {
+    menu.children(".dropdown-close-btn").hide();
+    menu.children(".dropdown-open-btn").show();
+    menu.children(".dropdown-menu").animate(
+      { height: 0 },
+      100
+    );
+  }
+
+  //메뉴 이름 초기화
+  function clearMenuText(menus) {
+    menus.each((i, menu) => {
+      let defaultText = $(menu).siblings("p").text();
+      $(menu).children("p").text("--- " + defaultText + " 선택 ---");
+    });
+  }
+
+  //전체 메뉴 초기화
+  function clearOptions(menus) {
+    closeMenu($(menus));
+
+    menus.removeClass("selectable");
+    menus.first().addClass("selectable");
+
+    clearMenuText(menus);
+  }
 }
 
 function productEvent() {
