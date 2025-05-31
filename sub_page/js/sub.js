e4517bf73795f35e05983a8832bfbb5c42f3c524
bini <bini020408@naver.com>
2025-05-31 16:11:57 +0900

    상품 옵션 정보 추가, 옵션 아이콘 클릭 이벤트 추가

--- sub_page/js/sub.js
+++ sub_page/js/sub.js
@@ -10,4 +10,20 @@ function productEvent() {
   product.on("click", function() {
     window.open("../detail_page/detail_page.html", "_blank").focus();
   });
+
+  //관심 아이콘 클릭 시 이동하지 않음
+  productIconList.children("li:nth-of-type(1)").on("click", function() {
+    return false;
+  });
+
+  //옵션 아이콘 클릭 시 옵션 표시
+  productIconList.children("li:nth-of-type(2)").on("click", function() {
+    $(this).parent().siblings(".product-option").toggle();
+    return false;
+  });
+
+  //상품에서 마우스가 나가면 옵션 비표시
+  product.on("mouseleave blur", function() {
+    $(this).find(".product-option").hide();
+  });
 }
