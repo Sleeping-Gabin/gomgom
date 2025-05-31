3d50c6f1d7b04015f8037eb2a26ae843a09967ef
bini <bini020408@naver.com>
2025-06-01 00:23:16 +0900

    장바구니 옵션 선택 시 선택된 옵션으로 텍스트 수정 기능 추가

--- js/cart.js
+++ js/cart.js
@@ -3,9 +3,12 @@ $(function() {
   const closeBtn = $(".modal-cart .modal-close-btn");
   const cartOption = $(".modal-cart .cart-container .cart-option");
   const dropdowns = $(".modal-cart .cart-container .cart-option .dropdown");
+  const dropdownList = $(".modal-cart .cart-container .cart-option .dropdown .dropdown-menu li");
 
   //닫기 버튼 클릭시 닫기
   closeBtn.on("click", function() {
+    clearOptions(dropdowns);
+
     cartBg.css("display", "none");
   });
 
@@ -22,6 +25,27 @@ $(function() {
     }
   });
 
+  //메뉴 선택시
+  dropdownList.on("click", function() {
+    const dropdown = $(this).parents(".dropdown");
+
+    let idx = dropdowns.index(dropdown);
+    let selectText = $(this).text();
+
+    //선택 메뉴 표시
+    dropdown.children("p").text(selectText);
+
+    if (idx < dropdowns.length-1) {
+      //다음 옵션을 선택 가능하게 하고, 이후 옵션들을 초기화
+      dropdowns.eq(idx+1).addClass("selectable");
+      dropdowns.slice(idx+2).removeClass("selectable");
+      clearMenuText(dropdowns.slice(idx+1));
+    }
+    else {
+      clearOptions(dropdowns);
+    }
+  });
+  
   //메뉴를 엶
   function openMenu(menu) {
     let menuHeight = menu.find(".dropdown-menu ul").outerHeight();
@@ -42,5 +66,23 @@ $(function() {
       { height: 0 },
       100
     );
+  }
+  
+  //메뉴 이름 초기화
+  function clearMenuText(menus) {
+    menus.each((i, menu) => {
+      let defaultText = $(menu).siblings("p").text();
+      $(menu).children("p").text("--- " + defaultText + " 선택 ---");
+    });
+  }
+  
+  //전체 메뉴 초기화
+  function clearOptions(menus) {
+    closeMenu($(menus));
+  
+    menus.removeClass("selectable");
+    menus.first().addClass("selectable");
+  
+    clearMenuText(menus);
   }
 });
